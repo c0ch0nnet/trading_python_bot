56717f53ed448858c76b796381bb7d6ed7f94071
Новиков Иван Иванович <ivan.novikov@sbermarket.ru>
2023-03-24 15:07:25 +0300

    finaly

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 
 # покупаем, когда позиция = 0. цена покупки - по рынку
 # в ином случае, продаем. цена продажи - цена покупки + маржа
+import random
+import time
 
-last_price = 90
-step = 1
-buy_price = last_price - step
-position_price = buy_price
-margin = 5
-sell_price = position_price + margin
-position_size = 1
 
-if position_size > 0:
-    print(f'Продать доллары по {sell_price}')
-else:
-    print(f'Купить доллары по {buy_price}')
+def get_last_price():
+    return random.randint(69, 90)
+
+def get_position():
+    return random.randint(0, 1), random.randint(69, 90)
+
+def get_open_order():
+    return random.randint(0, 2)
+
+while True:
+    time.sleep(15)
+    open_order = get_open_order()
+    last_price = get_last_price()
+    step = 1
+    buy_price = last_price - step
+    position_size, position_price = get_position()
+    margin = 5
+    sell_price = position_price + margin
+
+    print(f'Позиция: {position_price}@{position_size}. Цена последней сделки: {last_price}. '
+          f'Количество открытых ордеров = {open_order}')
+    if open_order > 0:
+        print(f'Уже есть открытые ордера')
+        continue
+    if position_size > 0:
+        print(f'Продать доллары по {sell_price}')
+    else:
+        print(f'Купить доллары по {buy_price}')
