5d4ef211ce1d1bd9fc391fbc2ea65ecdf7aeffeb
Новиков Иван Иванович <ivan.novikov@sbermarket.ru>
2023-03-24 16:11:38 +0300

    refactoring

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-
-# покупаем, когда позиция = 0. цена покупки - по рынку
-# в ином случае, продаем. цена продажи - цена покупки + маржа
 import random
 import time
 
@@ -8,28 +5,45 @@ import time
 def get_last_price():
     return random.randint(69, 90)
 
+
 def get_position():
     return random.randint(0, 1), random.randint(69, 90)
+
 
 def get_open_order():
     return random.randint(0, 2)
 
+
+def create_sell_order(price):
+    print(f'Продать доллары по {price}')
+
+
+def create_buy_order(price):
+    print(f'Купить доллары по {price}')
+
+
+def _log_message(message):
+    print(message)
+
+
+step = 1
+margin = 5
+sleep = 15
+
 while True:
-    time.sleep(15)
+    time.sleep(sleep)
     open_order = get_open_order()
     last_price = get_last_price()
-    step = 1
     buy_price = last_price - step
     position_size, position_price = get_position()
-    margin = 5
     sell_price = position_price + margin
 
-    print(f'Позиция: {position_price}@{position_size}. Цена последней сделки: {last_price}. '
-          f'Количество открытых ордеров = {open_order}')
+    _log_message(f'Позиция: {position_price}@{position_size}. Цена последней сделки: {last_price}. '
+                 f'Количество открытых ордеров = {open_order}')
     if open_order > 0:
-        print(f'Уже есть открытые ордера')
+        _log_message('Уже есть открытые ордера')
         continue
     if position_size > 0:
-        print(f'Продать доллары по {sell_price}')
+        create_sell_order(sell_price)
     else:
-        print(f'Купить доллары по {buy_price}')
+        create_buy_order(buy_price)
